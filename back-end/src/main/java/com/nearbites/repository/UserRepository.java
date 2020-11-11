39b95fcef048fedef762c9b4d4ea592c734a4f3f
elishaaquino <elishaaquino1998@gmail.com>
2020-11-11 11:30:19 -0800

    took out unused import

--- back-end/src/main/java/com/nearbites/repository/UserRepository.java
+++ back-end/src/main/java/com/nearbites/repository/UserRepository.java
@@ -3,7 +3,5 @@ package com.nearbites.repository;
 import com.nearbites.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface UserRepository extends MongoRepository<User, String> {
 }
